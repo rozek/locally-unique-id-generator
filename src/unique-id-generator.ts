82adbd4125085feaaa0aaaa9e3d9052c2b034c3c
Andreas Rozek <A.Rozek@gmx.de>
2021-06-26 06:20:05 +0200

    Create unique-id-generator.ts

---
+++ src/unique-id-generator.ts
@@ -0,0 +1,12 @@
+//----------------------------------------------------------------------------//
+//                            unique Id Generator                             //
+//----------------------------------------------------------------------------//
+
+  let IdCounter = 0
+
+  function newUniqueId ():string {
+    return 'uid-' + ++IdCounter
+  }
+
+  export default newUniqueId
+
