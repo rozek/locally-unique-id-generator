32a5ef2098302f823e6eaa9ade403871a2e499a3
Andreas Rozek <A.Rozek@gmx.de>
2021-07-06 13:01:32 +0200

    Update locally-unique-id-generator.ts

--- src/locally-unique-id-generator.ts
+++ src/locally-unique-id-generator.ts
@@ -2,10 +2,25 @@
 //                        locally unique Id Generator                         //
 //----------------------------------------------------------------------------//
 
-  let IdCounter = 0
+  let IdCounter = 0                         // hidden in the closure of "nextId"
+
+  function nextId ():number {            // a really unique incrementor function
+    return ++IdCounter
+  }
+
+/**** make global.nextId a real singleton ****/
+
+  const global = /*#__PURE__*/ Function('return this')()
+// see https://stackoverflow.com/questions/3277182/how-to-get-the-global-object-in-javascript
+
+  if (typeof global.nextId !== 'function') {
+    global.nextId = nextId
+  }
+
+/**** newUniqueId - the actually exported function ****/
 
   function newUniqueId ():string {
-    return 'uid-' + ++IdCounter
+    return 'uid-' + global.nextId()
   }
 
   export default newUniqueId
