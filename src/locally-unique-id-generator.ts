5e5b8b60c064505c59629b553be1e427e1d896de
Andreas Rozek <A.Rozek@gmx.de>
2021-07-07 07:32:11 +0200

    Update locally-unique-id-generator.ts

--- src/locally-unique-id-generator.ts
+++ src/locally-unique-id-generator.ts
@@ -13,14 +13,14 @@
   const global = /*#__PURE__*/ Function('return this')()
 // see https://stackoverflow.com/questions/3277182/how-to-get-the-global-object-in-javascript
 
-  if (typeof global.nextId !== 'function') {
-    global.nextId = nextId
+  if (typeof global.__nextId !== 'function') {
+    global.__nextId = nextId
   }
 
 /**** newUniqueId - the actually exported function ****/
 
   function newUniqueId ():string {
-    return 'uid-' + global.nextId()
+    return 'uid-' + global.__nextId()
   }
 
   export default newUniqueId
